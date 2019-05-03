b3fe8458f097ab356e1b1662e3bd5dcceea84612
Maarten Wong <jachtschipwong@hotmail.com>
2019-05-03 09:38:30 +0200

    Mostly testing, onclick event not functional yet

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-function CollectTheme {
-  let CurrentTheme = document.getelementbyclass('still-lifepainting').value;
-  localstorage.setitem("CurrentTheme", CurrentTheme);
+
+window.onload=function() {
+/*  var onClickPaintingOne = document.getElementById('s16'); */
+  var src0 = document.getElementById('s16').getAttribute('src');
+  window.localStorage.setItem('CurrentSrc', src0);
+/*  onClickPaintingOne.addEventListener('click', CollectTheme); */
+/*  onClickPaintingOne.addEventListener('click', CollectId); */
 }
-function CollectId {
-  let CurrentId= document.getelementbyid('s16').value;
-  localstorage.setitem("CurrentId", CurrentId);
-}
+
+/*function CollectTheme() {
+  console.log("function working");
+  let CurrentTheme = document.getElementsByClassName('still-lifepainting');
+  window.localStorage.setItem('CurrentTheme', CurrentTheme);
+} */
+/*function CollectId() {
+  console.log("function working");
+  let CurrentId = document.getElementById('s16');
+  window.localStorage.setItem('CurrentId', CurrentId);
+}*/
 /* TODO: Make it so that the src of the 17th image in paintings.html is the same as the src of the id of the clicked image on the home page.
 You can do this by localstorage hopefully, try asking discord */
